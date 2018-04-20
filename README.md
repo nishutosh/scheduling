6dff0a9c2765c827fcbcd2845ab323add28393d4
nishutosh <nishutosh002@gmail.com>
2018-04-20 19:58:14 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### Function in a module
+```python
+from schedule.availabiliy import *
+
+#to load json initially
+reload()
+
+#to update a record
+
+modify(index)
+
+```
+
---
+++ availability.py
@@ -0,0 +1,43 @@
+import openpyxl
+import json
+from datetime import datetime
+import calendar
+
+
+current_time=datetime.now()
+
+workbook = openpyxl.load_workbook('Schedule.xlsx')
+worksheet = workbook[calendar.day_name[current_time.weekday()]]
+def reload():
+  """ reset all data"""	
+  outfile=open('availability.json', mode='w')
+  status_upload={}
+  for row in worksheet.iter_rows(min_row=2, min_col=0, max_col=1):
+      for cell in row:
+          print(cell.value)
+          status_upload[str(cell.value)]="present"
+  json.dump(status_upload, outfile)
+  outfile.close()
+
+
+def modify(index):
+  """
+  enter index of the record to be changed
+  """	
+  file_read=open("availability.json",mode="r")	
+  check_availability=json.load(file_read) 
+  if check_availability[str(index)]=="present":
+      check_availability[str(index)]="absent"
+  elif check_availability[str(index)]=="absent":
+      check_availability[str(index)]="present"
+  print(check_availability)
+  file_read.close()
+  file_write=open("availability.json",mode="w+")	  
+  json.dump(check_availability,file_write)
+
+
+
+
+
+
+
